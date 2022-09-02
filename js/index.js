65834a21643c0d8e3e48939b919e40c1680f2f11
Anamul10 <anamul.hasan@mnsu.edu>
2022-09-02 17:37:46 -0400

    added news id and connected display all news

--- js/index.js
+++ js/index.js
@@ -1,24 +1,46 @@
-const loadnewsCategory = () => {
+const loadNewsCategory = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
-        .then(news => displayData(news.data.news_category))
+        .then(data => displayData(data.data.news_category))
 }
 
 const displayData = categories => {
     const categoriesContainer = document.getElementById('categories');
     categories.forEach(category => {
-        console.log(category);
+        // console.log(category);
         const categoryDiv = document.createElement('div');
         categoryDiv.classList.add('col');
         categoryDiv.innerHTML = `
-        <button type="button" class="btn btn-light">${category.category_name}</button>
+        <button onclick="loadNewsCategoryId('${category.category_id}')" class="btn btn-light">${category.category_name}</button>
         `;
         categoriesContainer.appendChild(categoryDiv);
-    })
+    });
 
 }
 
+const loadNewsCategoryId = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
+        .then(res => res.json())
+        .then(data => displayAllNews(data.data))
+}
 
-loadnewsCategory();
+const displayAllNews = blogs => {
+    console.log(blogs);
+    /* const blogContainer = document.getElementById('blog-container');
+    blogs.forEach(blog => {
+        console.log(blog);
+    }) */
+}
+
+
+
+
+
+displayAllNews();
+
+loadNewsCategoryId();
+
+loadNewsCategory();
 
 displayData();
+
