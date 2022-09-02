97c585f18df8f8f4d3af0af5bc6fc0611a42ea05
Anamul10 <anamul.hasan@mnsu.edu>
2022-09-02 15:38:40 -0400

    added category section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('test')
+const loadnewsCategory = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(news => displayData(news.data.news_category))
+}
+
+const displayData = categories => {
+    const categoriesContainer = document.getElementById('categories');
+    categories.forEach(category => {
+        console.log(category);
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('col');
+        categoryDiv.innerHTML = `
+        <button type="button" class="btn btn-light">${category.category_name}</button>
+        `;
+        categoriesContainer.appendChild(categoryDiv);
+    })
+
+}
+
+
+loadnewsCategory();
+
+displayData();
